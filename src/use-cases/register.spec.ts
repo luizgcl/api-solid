b56df151ad3133105d738d27a42756512945ca8b
luizgcl <luiizgcl@gmail.com>
2023-03-15 22:19:13 -0300

    chore: config test coverage

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -48,7 +48,7 @@ describe('Register Use Case', () => {
       password: '123456',
     })
 
-    expect(() =>
+    await expect(() =>
       registerUseCase.handle({
         name: 'Jhon Doe',
         email,
