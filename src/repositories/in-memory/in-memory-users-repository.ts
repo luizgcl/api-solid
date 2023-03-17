85c1c584d8030fc3ff36c84bcb0742f21e385a36
luizgcl <luiizgcl@gmail.com>
2023-03-17 07:49:23 -0300

    chore: check-in use case

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,11 +1,12 @@
 import { User, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
 import { UsersRepository } from './../users-repository'
 
 export class InMemoryUsersRepository implements UsersRepository {
-  public users: User[] = []
+  public items: User[] = []
 
   async findById(id: string) {
-    const user = this.users.find((user) => user.id === id)
+    const user = this.items.find((user) => user.id === id)
 
     if (!user) return null
 
@@ -13,7 +14,7 @@ export class InMemoryUsersRepository implements UsersRepository {
   }
 
   async findByEmail(email: string) {
-    const user = this.users.find((user) => user.email === email)
+    const user = this.items.find((user) => user.email === email)
 
     if (!user) return null
 
@@ -22,14 +23,14 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'test-user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
       created_at: new Date(),
     }
 
-    this.users.push(user)
+    this.items.push(user)
 
     return user
   }
