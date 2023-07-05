f591d68ccb7be391bdcbd4d2d7bdec607f01a464
luizgcl <luiz.leme@madelife.med.br>
2023-07-04 23:56:31 -0300

    chore: refresh token controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,10 +5,13 @@ import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { authenticate } from './authenticate.controller'
 import { register } from './register.controller'
 import { profile } from './profile.controller'
+import { refresh } from './refresh.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
