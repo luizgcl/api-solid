0506d72ecf03502732d46466b7506a5b0c73ddda
luizgcl <luiz.leme@madelife.med.br>
2023-07-04 22:19:04 -0300

    chore: validate check-in controller

---
+++ src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,19 @@
+import { makeValidateCheckInCase } from '@/use-cases/factories/make-validate-check-in-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function validate(request: FastifyRequest, reply: FastifyReply) {
+  const validateCheckInParamsSchema = z.object({
+    checkInId: z.string().uuid(),
+  })
+
+  const { checkInId } = validateCheckInParamsSchema.parse(request.params)
+
+  const validateCheckInUseCase = makeValidateCheckInCase()
+
+  validateCheckInUseCase.handle({
+    checkInId,
+  })
+
+  return reply.status(204).send()
+}
